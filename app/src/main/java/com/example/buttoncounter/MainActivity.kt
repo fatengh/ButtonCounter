f8d42fcce527ea32ff36f3fe3a1bbec803ea6bc8
faten.ghamdi <Faten.arg@hotmail.com>
2021-10-18 17:27:06 +0300

    Initial commit

---
+++ app/src/main/java/com/example/buttoncounter/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.buttoncounter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var tv: TextView
+
+    private lateinit var btnp: Button
+    private lateinit var btnm: Button
+
+    var counter = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tv = findViewById(R.id.textView)
+
+        btnp = findViewById(R.id.btnp)
+        btnm = findViewById(R.id.btnm)
+
+
+        btnp.setOnClickListener {
+            plus()
+        }
+        btnm.setOnClickListener {
+            minse()
+        }
+
+    }
+
+    fun plus() {
+        counter++
+        tv.text = counter.toString()
+    }
+
+    fun minse() {
+        counter--
+        tv.text = counter.toString()
+    }
+}
